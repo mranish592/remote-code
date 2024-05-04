8978c13a772f63cef0fc8d766b86d228619b0908
Anish\ Gupta <mranish592@gmail.com>
2024-05-04 16:20:58 +0530

    fix: css path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { v4 as uuidv4 } from "uuid";
-import "./App.css";
+
 import { CollaboratePage } from "./components/CollaboratePage";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 function App() {
