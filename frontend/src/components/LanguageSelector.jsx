d142b35c51f0aa9bd2e5b31ef0b4d2d7f56016d0
Anish\ Gupta <mranish592@gmail.com>
2024-06-17 22:35:53 +0530

    fix: remove C++ option from language selector

--- frontend/src/components/LanguageSelector.jsx
+++ frontend/src/components/LanguageSelector.jsx
@@ -16,7 +16,6 @@ function LanguageSelector({ className, setLanguage }) {
                 className="text-sm rounded-lg w-full p-2 bg-gray-700 border border-gray-600 placeholder-gray-400 text-white focus:ring-primary focus:border-primary hover:bg-slate-500"
             >
                 <option value="javascript">javascript</option>
-                <option value="cpp">C++</option>
             </select>
         </div>
     );
