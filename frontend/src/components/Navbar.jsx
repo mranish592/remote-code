ec29d87110971462bcc98fefd747162a62821b17
Anish\ Gupta <mranish592@gmail.com>
2025-02-16 20:44:03 +0530

    feat: add github logo for readme

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,13 @@
+import githubLogo from "../assets/github.png";
 function Navbar({ className }) {
     return (
-        <div className={`${className} flex items-center`}>
+        <div className={`${className} flex items-center justify-between`}>
             <div>
                 <h1 className="mx-14 my-4 font-semibold font-sans text-xl">Remote Code</h1>
             </div>
+            <a href="https://github.com/mranish592/remote-code/blob/main/README.md">
+                <img src={githubLogo} alt="github" className="h-8 w-8 mx-14" />
+            </a>
         </div>
     );
 }
